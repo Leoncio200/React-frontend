1551a7c09fc85cddd24208fbf93b9664cc229396
Leoncio <leonciopimentelperez@gmail.com>
2023-12-28 16:00:50 -0600

    search box add

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import { IoMenu } from "react-icons/io5";
 import { AiOutlineClose } from "react-icons/ai";
-import { FaUserCircle } from "react-icons/fa";
+import { FaUserCircle, FaSearch } from "react-icons/fa";
 import "./navbar.css";
 import { Link, useNavigate } from 'react-router-dom';
 
-export default function navbar() {
+export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     const navigate = useNavigate();
 
@@ -20,6 +20,10 @@ export default function navbar() {
   return (
     <div className='navbar'>
         <div className="logo"><Link to='/' className='li'>Japan World</Link></div>
+        <div className="search">
+            <input type="text"  placeholder='Buscar...'/>
+            <FaSearch className='search-icon'/>
+        </div>
         <ul className='links'>
             <Link to='/' className='li'>Home</Link>
             <Link to='/' className='li'>Contactos</Link>
